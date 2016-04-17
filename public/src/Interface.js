718498d06e4d602f4e5fed077ee5dc6593211776
Anne <annemarie.k89@gmail.com>
2016-04-17 21:47:29 +0100

    changed to using Knockout.js

---
+++ public/src/Interface.js
@@ -0,0 +1,45 @@
+/* eslint-env knockout.js */
+
+const inputGrid = function inputGrid() {
+  return [[undefined, 6, undefined, undefined, undefined, undefined, 1, undefined],
+          [undefined, undefined, undefined, 6, 5, 1, undefined, undefined, undefined],
+          [1, undefined, 7, undefined, undefined, undefined, 6, undefined, 2],
+
+          [6, 2, undefined, 3, undefined, 5, undefined, 9, 4],
+          [undefined, undefined, 3, undefined, undefined, undefined, 2, undefined, undefined],
+          [4, 8, undefined, 9, undefined, 7, undefined, 3, 6],
+
+          [9, undefined, 6, undefined, undefined, undefined, 4, undefined, 8],
+          [undefined, undefined, undefined, 7, 9, 4, undefined, undefined, undefined],
+          [undefined, 5, undefined, undefined, undefined, undefined, undefined, 7, undefined]];
+};
+
+const viewModel = function viewModel(inputGrid) {
+  const grid = [];
+  let row = 0;
+  const len = 9;
+  const populateRow = function populateRow(rowNum) {
+    const newRow = [];
+    let col = 0;
+    for (;col < len; col++) {
+      newRow.push({
+        val: ko.observable(inputGrid[rowNum][col]),
+        disabled: (inputGrid[rowNum][col])
+      });
+    }
+    return newRow;
+  };
+  for (;row < len; row++) {
+    grid.push(populateRow(row));
+  }
+  return {
+    grid: grid,
+      submit: function(args) {
+        console.log(args);
+      },
+  };
+};
+
+
+const thisvm = viewModel(inputGrid());
+ko.applyBindings(thisvm, document.getElementById('container'));
